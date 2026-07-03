6fb67d5c59b83038ee6e6011f1418a2c328d894f
VishaaliS <vishaalitech@gmail.com>
2026-07-03 22:28:02 +0530

    Add files via upload

---
+++ Week-2/Day-11/02_single_number.java
@@ -0,0 +1,7 @@
+// Problem: Single Number
+// LeetCode 136: https://leetcode.com/problems/single-number/
+// Difficulty: Easy
+// Pattern: XOR Bit Manipulation
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Date: July 4, 2025 (Revisit)
