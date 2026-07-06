806ad6409c5f99dbb620c80da4fef1588319ec75
VishaaliS <vishaalitech@gmail.com>
2026-07-07 02:46:25 +0530

    Add files via upload

---
+++ Week-3/Day-14/03_longest_substring_without_repeating.java
@@ -0,0 +1,36 @@
+// Problem: Longest Substring Without Repeating Characters
+// LeetCode 3: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Difficulty: Medium
+// Pattern: Sliding Window + HashSet
+// Time Complexity: O(n)
+// Space Complexity: O(min(n, charset))
+// Date: July 6, 2025
+//
+// APPROACH:
+// Variable window with HashSet
+// Track characters in current window
+// Shrink when duplicate found
+// Return max window size
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map <Character , Integer> ans = new HashMap<>();
+        
+        int i = 0;
+        int j = 0;
+        int maxcount = 0;
+        while(i < s.length()){
+
+                while(ans.containsKey(s.charAt(i))){
+                    ans.remove(s.charAt(j));
+                    j++;
+                }
+            
+           
+            ans.put(s.charAt(i),i);
+             maxcount = Math.max(maxcount , i-j+1);
+            i++;
+        }
+     return maxcount;
+    }
+}
