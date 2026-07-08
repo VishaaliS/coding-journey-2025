2db2205da3b1118341cde5d209a099b6096ae9a8
VishaaliS <vishaalitech@gmail.com>
2026-07-08 23:30:03 +0530

    Add files via upload

---
+++ Week-3/Day-16/01_merge_two_sorted_lists.java
@@ -0,0 +1,56 @@
+// Problem: Merge Two Sorted Lists
+// LeetCode 21: https://leetcode.com/problems/merge-two-sorted-lists/
+// Difficulty: Easy
+// Pattern: Dummy Node + Two Pointer
+// Time Complexity: O(n+m)
+// Space Complexity: O(1)
+// Date: July 8, 2025
+//
+// APPROACH:
+// Create dummy node
+// Compare nodes from both lists
+// Attach smaller node to result
+// Move pointer forward
+// Return dummy.next
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode newnode = new ListNode();
+        ListNode head = newnode;
+       
+       …  newnode = newnode.next;
+           }
+           else{
+            
+            newnode.next = temp2;
+            temp2 = temp2.next;
+            newnode = newnode.next;
+           }
+       } 
+       while(temp1 != null){
+        
+        newnode.next = temp1;
+        temp1 = temp1.next;
+        newnode = newnode.next;
+       }
+       while(temp2 != null){
+        
+        newnode.next = temp2;
+        temp2 = temp2.next;
+        newnode = newnode.next;
+       }
+       return head.next;
+    }
+}
